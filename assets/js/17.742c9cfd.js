7ee7422428213203bbf5d9cbd7f56647ad5222fa
padosum <makalu171@gmail.com>
2021-03-18 16:02:01 +0900

    🗑 Delete deploy files

--- assets/js/17.742c9cfd.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{488:function(n,w,o){}}]);
--- assets/js/18.d6bf54dc.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{489:function(n,w,o){}}]);
