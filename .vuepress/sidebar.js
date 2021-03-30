a9e93b32743d9a5e40e9a76ddbfc69f0b9e77207
padosum <makalu171@gmail.com>
2021-03-30 22:01:48 +0900

    🛠️ fix sidebar

--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -5,39 +5,5 @@ module.exports = {
         "object",
         "set-and-map",
     ],
-    "/": [
-    "",
-    {
-        title: "Guide",
-        icon: "creative",
-        prefix: "guide/",
-        collapsable: false,
-        children: [
-        "get-started/",
-        "interface/",
-        "layout/",
-        "markdown/",
-        "feature/",
-        "blog/",
-        "eject",
-        ],
-    },
-    {
-        title: "Config",
-        icon: "config",
-        prefix: "config/",
-        collapsable: false,
-        children: ["", "theme/", "page", "stylus", "plugin/"],
-    },
-    {
-        title: "Basic",
-        icon: "module",
-        prefix: "basic/",
-        collapsable: false,
-        children: ["tutorial", "markdown/", "vuepress/"],
-    },
-    "FAQ/",
-    "demo/",
-    ],
 };
   
